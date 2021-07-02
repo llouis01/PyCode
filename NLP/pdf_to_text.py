384cc33e403980083f23e11c036b75ebd718f1f0
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2021-07-02 10:43:51 -0400

    Update pdf_to_text.py

--- NLP/pdf_to_text.py
+++ NLP/pdf_to_text.py
@@ -9,9 +9,20 @@ import nltk
 import pandas as pd
 import PyPDF2 as PDF2
 
-# import and process pdf file 
+#### import and process pdf file 
 file1 = open("SPC_Louis_Range_OPORD.pdf", "rb")
-reader = PDF2.PdfFileReader(file1)
 
-for page in reader.getNumPages:
-    reader.getPage(page).extractText()
+
+reader = PDF2.PdfFileReader(file1) # reader
+
+# import file 1-by-1
+pp1 = reader.getPage(0).extractText()
+pp2 = reader.getPage(1).extractText()
+pp3 = reader.getPage(2).extractText()
+pp4 = reader.getPage(3).extractText()
+
+# read multiple pages iteratively
+pages = []
+for i in range(reader.getNumPages()):
+    pages[i] = reader.getPage(i).extractText()
+    print(reader.getPage(i).extractText())
