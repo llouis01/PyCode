93b0392048fb228b8f19cb42b3150ccd3817deac
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2021-07-02 12:59:25 -0400

    Update pdf_to_text.py

--- NLP/pdf_to_text.py
+++ NLP/pdf_to_text.py
@@ -6,6 +6,7 @@ Created on Fri Jul  2 09:59:33 2021
 """
 
 import nltk
+import os
 import pandas as pd
 import PyPDF2 as PDF2
 
@@ -16,10 +17,10 @@ file1 = open("SPC_Louis_Range_OPORD.pdf", "rb")
 reader = PDF2.PdfFileReader(file1) # reader
 
 # import file 1-by-1
-pp1 = reader.getPage(0).extractText()
-pp2 = reader.getPage(1).extractText()
-pp3 = reader.getPage(2).extractText()
-pp4 = reader.getPage(3).extractText()
+# pp1 = reader.getPage(0).extractText()
+# pp2 = reader.getPage(1).extractText()
+# pp3 = reader.getPage(2).extractText()
+# pp4 = reader.getPage(3).extractText()
 
 # read multiple pages iteratively
 # pages = []
@@ -27,18 +28,24 @@ pp4 = reader.getPage(3).extractText()
 #     pages[i] = reader.getPage(i).extractText()
 #     print(reader.getPage(i).extractText())
     
-def pdf_to_text(pdf_file):
-    file_content = []
+def pdf_to_text(pdf_file, output_txt):
+    file_content = [] # list to save text
+    
     # import pdf file, extract text and pass into a .txt
     pdf_reader = PDF2.PdfFileReader(pdf_file)
     
     # get page and content
     for i in range(pdf_reader.getNumPages()):
         page = pdf_reader.getPage(i)
-        p_content = page.extractText().replace("\n", "")
+        p_content = page.extractText()
         file_content.append(p_content)
+    # return file_content
+
+    # save to text file
+    with open(output_txt, "w") as output:
+        output.write("\n".join([i for i in file_content]))
         
 
 
-
-pdf_to_text("SPC_Louis_Range_OPORD.pdf")
+os.chdir(r"C:\Users\Loube\Desktop\OCS Packet")
+pdf_to_text("SPC_Louis_Range_OPORD.pdf", "pdf_content.txt")
